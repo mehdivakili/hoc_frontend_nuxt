99046faea0369de5d7816c8e54dd056e79d1a5b8
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-04-27 00:04:40 +0430

    migrating to vuetify

--- components/Header.vue
+++ components/Header.vue
@@ -1,26 +1,43 @@
 <template>
-  <nav>
-    <div>
+  <v-app-bar app class="nav d-flex justify-space-between align-center pa-4">
+    <div class="">
       <img src="../assets/images/logo.png" alt="" />
-      <nuxt-link to="">خانه</nuxt-link>
-      <nuxt-link to="">اعتبار سنجی مدرک</nuxt-link>
-      <nuxt-link to="">زنگ برنامه نویسی دروه ششم</nuxt-link>
+      <nuxt-link class="px-4" to="">خانه</nuxt-link>
+      <nuxt-link class="px-4" to="">اعتبار سنجی مدرک</nuxt-link>
+      <nuxt-link class="px-4" to="">زنگ برنامه نویسی دروه ششم</nuxt-link>
     </div>
-    <div>
-      <v-btn>ثبت نام</v-btn>
-      <v-btn>ورود</v-btn>
+    <div class="d-flex">
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        class="d-flex d-sm-none"
+      ></v-app-bar-nav-icon>
+      <v-btn class="button-fill">ثبت نام</v-btn>
+      <div class="mx-4"></div>
+      <v-btn class="button-outline">ورود</v-btn>
     </div>
-  </nav>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
-nav {
+.nav {
+  width: 100%;
   z-index: 2;
+  background-color: white;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 }
 </style>
