bda28da817b29aaed0339a239a46258c9ed2d670
Hosseinreza <hrakbari77@gmail.com>
2022-05-06 11:15:34 +0430

    Fix Section 3

--- components/Register/UserForm.vue
+++ components/Register/UserForm.vue
@@ -1,19 +1,47 @@
 <template>
-  <v-row justify="center">
-    <v-card outlined  elevation="15" class="con">
-      <v-card-text >
+  <div>
+    <v-row justify="center" class="topRow">
+        <v-col cols="3">
 
-      <p style="text-align: center; font-weight: bold;">برای ادامه ثبت نام حتما<br />
-        در پرسشنامه شرکت کنید.</p>
+        </v-col>
+      <v-col cols="6" style="font-family: 'Vazir';
+                        font-style: normal;
+                        font-weight: 700;
+                        font-size: 0.80em;
+                        line-height: 25px;
+                        color: #004948;"
+      >
+        <p>عکس کارت واکسن:</p>
+        <v-file-input
+          class="inputField"
 
-      </v-card-text>
+          show-size
+
+        ></v-file-input>
+      </v-col>
+      <v-col cols="3">
+
+      </v-col>
 
 
-        <v-btn to="/register/form/" class="button-fill" style="width: 70%;margin-right: 2.5em;margin-bottom: 2em">ورود به پرسشنامه</v-btn>
+    </v-row>
+    <v-row justify="center" class="botRow">
+
+      <v-card outlined  elevation="15" class="con">
+        <v-card-text >
+
+        <p style="text-align: center;color:#004948; font-weight: bold;">برای ادامه ثبت نام حتما<br />
+          در پرسشنامه شرکت کنید.</p>
+
+        </v-card-text>
 
 
-    </v-card>
-  </v-row>
+          <v-btn to="/register/form/" class="button-fill" style="width: 70%;margin-right: 2.5em;margin-bottom: 2em">ورود به پرسشنامه</v-btn>
+
+
+      </v-card>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -29,5 +57,15 @@ export default {
   background: #C7E4E4;
   border-radius: 8px;
 
+}
+.inputField{
+  direction:ltr !important;
+}
+.topRow{
+  margin-bottom: -5em;
+  margin-right: 3em;
+}
+.botRow{
+  margin-right: 1em;
 }
 </style>
