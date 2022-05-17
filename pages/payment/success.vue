77eff614f5febb278a7a12648ab4efe73715a6dd
mehdivakili <0mvakilin0@gmail.com>
2022-05-17 14:38:42 +0430

    change success payment text

--- pages/payment/success.vue
+++ pages/payment/success.vue
@@ -1,5 +1,16 @@
 <template>
-  <p><img src="~/assets/images/success_icon.svg"/> ثبت نام شما با موفقیت انجام شد</p>
+  <div>
+    <p><img src="~/assets/images/success_icon.svg"/> ثبت نام شما با موفقیت انجام شد</p>
+    <ul>
+      <li>تاریخ برگزاری همایش<br>
+        ۲۵ تا ۳۱ تیرماه ۱۴۰۱
+      </li>
+      <li>به زودی به کانال واتساپ اطلاع رسانی همایش<br>
+        اضافه می شوید!
+      </li>
+      <li>در همایش منتظرتان هستیم!</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+div {
+  background: #C5E3E3;
+  box-shadow: inset -18px -18px 30px #E0F0F0, inset 18px 18px 30px #98CDCD;
+  border-radius: 15px;
+  padding: 30px 20px;
+
+  p {
+    font-size: 24px;
+    line-height: 45px;
+    text-align: center;
+    font-weight: 900;
+  }
+  ul {
+    padding-right: 30px;
+  }
+
+  /* identical to box height, or 189% */
+
+
+}
 </style>
