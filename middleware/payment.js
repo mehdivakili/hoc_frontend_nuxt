de3fe634e0d6dcf804eee9e238ea9fe62ca276c1
mehdivakili <0mvakilin0@gmail.com>
2022-05-24 12:05:05 +0430

    bug fix

--- middleware/payment.js
+++ middleware/payment.js
@@ -1,6 +1,6 @@
 export default function ({store, redirect, route}) {
   if (!route.path.startsWith('/register') && store.state.auth.loggedIn && !store.state.auth.user.is_purchased) {
     return redirect('/register')
-  } else if (route.path.startsWith('/register') && !store.state.auth.loggedIn && store.state.register.state === 4)
+  } else if (route.path.startsWith('/register') && !store.state.auth.loggedIn && store.state.register.state === 5)
     return redirect('/')
 }
