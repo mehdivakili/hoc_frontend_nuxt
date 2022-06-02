1da2daa7cff967aa3ce6aa096db50e11ad9e6291
mehdivakili <0mvakilin0@gmail.com>
2022-06-03 02:29:10 +0430

    add register video to home page

---
+++ components/home/VideoBanner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="banner" :style="{flexDirection: direction,background: bgColor}">
+    <div class="banner-content" :style="{color: textColor}">
+      <h3>
+        <slot name="banner-title"></slot>
+      </h3>
+      <p>
+        <slot name="banner-content" :style="{color: textColor}"></slot>
+      </p>
+    </div>
+    <div class="banner-img img-fluid">
+      <video controls>
+        <source :src="video" type="video/mp4">
+
+      </video>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  props: {
+    bgColor: {}, textColor: {}, video: {}, alt: {default: ''}, direction: {
+      default: 'row'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+.banner {
+  width: 100%;
+  margin: 0;
+}
+
+.banner-content, .banner-img {
+  position: relative;
+}
+
+.banner-img video {
+
+  width: 100% !important;
+}
+
+
+
+.banner-content {
+  text-align: justify;
+  padding: 20px;
+}
+
+.banner-content h3 {
+  margin-bottom: 30px;
+}
+
+@media screen and (min-width: 1000px) {
+  .banner-content, .banner-img {
+    width: 50% !important;
+
+  }
+
+
+  .banner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 110px;
+    gap: 110px;
+    height: 684px;
+
+  }
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -40,21 +40,31 @@
       </template>
     </Banner>
 
-    <Banner text-color="#004948" bg-color="#FEFEFE" :image="require('~/assets/images/banner3.png')" alt="یک عکس">
+
+    <Banner text-color="#004948" bg-color="#FEFEFE" :image="require('~/assets/images/banner1.png')" alt="یک عکس">
       <template v-slot:banner-title>
-        زنگ برنامه نویسی دانشگاه شیراز
+        چرا برنامه نویسی یاد بگیریم ؟
       </template>
 
       <template v-slot:banner-content>
-        بخش کامپیوتر دانشگاه شیراز یکی از برگزارکنندگان رویداد جهانی زنگ برنامه نویسی در ایران است.
-        دانشجویان این بخش از سال ۱۳۹۳ تا به حال به مدت شش دوره این رویداد را برای دانش‌آموزان ۱۲ تا ۱۸ سال برگزار کرده‌اند
-        و مجموعه‌هایی از جمله کافه‌‌بازار، مکتب‌خونه و … در سال‌های مختلف حامیان این رویداد بود‌ه‌اند.
-        <br>
-        امسال نیز دانشگاه شیراز برای هفتمین بار میزبان همۀ دانش‌آموزانی است که به آشنایی با برنامه نویسی علاقه‌مند هستند.
-        هدف این همایش ایجاد فرصتی برای دانش‌آموزان دوره متوسطه است تا به یادگیری مفاهیم پایهٔ برنامه نویسی در فضای دانشگاه بپردازند.
-        همچنین به شرکت‌کنندگان زنگ برنامه نویسی دانشگاه شیراز، مدرک بین‌المللی داده می‌شود که نشان می‌دهد جزو میلیون‌ها دانش‌آموزی هستند که در این رویداد جهانی شرکت داشته‌اند.
+        با پیشرفت تکنولوژی در قرن بیست‌و‌یکم، آشنایی با دانش کامپیوتر و برنامه‌ نویسی به یک مهارت ضروری تبدیل شده است. یادگیری برنامه‌ نویسی علاوه بر اینکه به ما این امکان را می‌دهد که ایده‌های خود را به واقعیت تبدیل کنیم، تفکر منطقی و حل مسئله را نیز به ما می‌آموزد.
+        درک برنامه نویسی به ما کمک می‌کند هنگام مواجه شدن با مشکلات، برای رسیدن به بهترین نتیجه به تمامی روش‌های حل مسئله فکر کنیم و هر بار که با چالش جدیدی روبرو می‌شویم، به‌ دنبال راه‌‌‌حل‌های متفاوت باشیم تا در نهایت به جواب درست برسیم.
+        بنابراین یادگیری برنامه‌ نویسی، طرز تفکر منطقی، خلاقیت و همچنین ذهنیت حل‌ مسئلهٔ ما را تقویت می‌کند؛ مهارت‌هایی که نه‌ تنها در زمینهٔ کاری به ما کمک می‌کنند، بلکه در زندگی و هنگام مواجهه با مسائل مختلف نیز مفید هستند. برنامه‌ نویسی به ما کمک می‌کند که مشکلات را از زاویهٔ جدیدی ببینیم، با استفاده از خلاقیت خود برای مشکلات دنیای واقعی راه‌حل‌های نوآورانه ارائه دهیم و جهان پیرامون خود را به مکان بهتری تبدیل کنیم.
+
       </template>
     </Banner>
+
+    <VideoBanner text-color="#FEFEFE" bg-color="linear-gradient(104.71deg, #00928F 11.26%, #00ADAA 90.55%)"
+                 direction="row-reverse" video="https://api.hocshirazu.ir/media/register_description.mp4" alt="یک فیلم">
+      <template v-slot:banner-title>
+        نحوه ثبت نام ؟!
+      </template>
+
+      <template v-slot:banner-content>
+
+        اگر هنگام ثبت نام براتون مشکل یا سوالی پیش اومد میتونید از طریق بشتیبان
+        سایت دایرکت اینستاگرام و بشتیبان تلگرام با ما درمیون بگذارید. </template>
+    </VideoBanner>
 
 <!--    <TeamTab2/>-->
     <TeamTab/>
@@ -68,6 +78,7 @@
 
 import MainBanner from "@/components/home/MainBanner";
 import Banner from "@/components/home/Banner";
+import VideoBanner from "@/components/home/VideoBanner";
 import Questions from "@/components/home/Questions";
 import TeamTab from "@/components/home/TeamTab";
 import Statistics from "@/components/home/Statistics";
@@ -77,7 +88,7 @@ export default {
   components: {
     Statistics,
     MainBanner,
-    Banner, Questions, TeamTab,TeamTab2,
+    Banner, Questions, TeamTab,TeamTab2,VideoBanner
   },
   auth: false,
   mounted() {
