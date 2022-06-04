39f980d303f85bf6a4156f638032b98edf12257a
mehdivakili <0mvakilin0@gmail.com>
2022-06-04 16:32:10 +0430

    init page builder

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,18 +67,29 @@ export default {
     // https://go.nuxtjs.dev/pwa
     // '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    // '@nuxt/content',
+    '@nuxt/content',
 
     '@nuxtjs/auth-next',
+    '@nuxtjs/markdownit',
+
+    "nuxt-compress"
 
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://api.hocshirazu.ir/api/',
+    baseURL: 'http://localhost:8000/api/',
   },
-
+  markdownit: {
+    preset: 'default',
+    linkify: true,
+    breaks: true,
+    use: [
+      'markdown-it-attrs'
+    ],
+    runtime: true // Support `$md()`
+  },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
---
+++ pages/pages/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-html="content"/>
+</template>
+
+<script>
+export default {
+  async asyncData({params, $axios}) {
+    let data = await $axios.$get('pages/' + params.slug)
+    data.content = data.content.replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, "\"");
+    return data
+  },
+  auth: false
+}
+</script>
+
+<style scoped>
+
+</style>
