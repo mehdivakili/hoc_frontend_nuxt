9dd629fe85a479cc66e3829e15bfc06317e86c95
Hosseinreza <hrakbari77@gmail.com>
2022-06-09 20:16:43 +0430

    Add quiz.vue

--- pages/account/quiz.vue
+++ pages/account/quiz.vue
@@ -20,16 +20,42 @@
           <v-card class="mainCard">
             <v-row>
               <v-col>
-                <v-card class="cardShape">
-
+                <v-card class="cardShape" style="margin-left: 2rem; margin-right: 2rem;margin-top: 2rem;margin-bottom: 2rem">
+                  <v-row> <!-- 1 -->
+                    <v-col>
+                      <p class="mainText">نوع کوییز: <span class="innerText">کوییز پیش فرض</span></p>
+                    </v-col>
+                  </v-row>
+                  <v-row> <!-- 2 -->
+                    <v-col>
+                      <p style="margin-top: -2rem" class="mainText">زمان کوییز:  <span class="innerText">۵ دقیقه</span></p>
+                    </v-col>
+                  </v-row>
+                  <v-row> <!-- 3 -->
+                    <v-col>
+                      <p style="margin-top: -2rem" class="mainText">تعداد سوالات:  <span class="innerText">۵ سوال</span></p>
+                    </v-col>
+                  </v-row>
+                  <v-row> <!-- 4 -->
+                    <v-col>
+                      <p style="margin-top: -2rem" class="mainText">نوع سوالات:  <span class="innerText">تستی</span></p>
+                    </v-col>
+                  </v-row>
+                  <v-row> <!-- 5 -->
+                    <v-col>
+                      <p style="margin-top: -2rem" class="mainText">نوع کوییز: <span class="innerText">کوییز پیش فرض</span></p>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <v-btn class="edit_btn" @click="participate" style="margin-top: -2rem">شرکت در کوییز</v-btn>
+                    </v-col>
+                  </v-row>
                 </v-card>
               </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
 
-              </v-col>
             </v-row>
+
           </v-card>
 
         </v-col>
@@ -40,7 +66,17 @@
 
 <script>
 export default {
-  name: "quiz"
+  name: "quiz",
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    participate(){
+      this.$router.push('/account/quizForm')
+    }
+  }
 }
 </script>
 
@@ -79,5 +115,28 @@ h2{
 
   box-shadow: -18px -18px 30px #E0F0F0, 18px 18px 30px #98CDCD;
   border-radius: 15px;
+}
+.mainText{
+  color: #004948;
+  font-family: 'Vazir';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+}
+.innerText{
+  font-weight: 900 !important;
+}
+.edit_btn {
+  box-shadow: inset 0px -4px 4px rgba(255, 252, 252, 0.25);
+  filter: drop-shadow(0px 8px 10px rgba(0, 146, 143, 0.25));
+  background: linear-gradient(180deg, #03B9B5 0%, #009592 100%);
+  border-radius: 15px;
+  padding: 10px 15px;
+  border: none;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 28px;
+  color: #FEFEFE;
+  margin-top: 20px;
 }
 </style>
