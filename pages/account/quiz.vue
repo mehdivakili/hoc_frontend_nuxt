6d0f591e6304b769a4611cfef2f505ad65b3e965
mehdivakili <0mvakilin0@gmail.com>
2022-06-17 19:24:53 +0430

    connect quiz to api

--- pages/account/quiz.vue
+++ pages/account/quiz.vue
@@ -1,12 +1,12 @@
 <template>
 
   <div>
-    <v-container >
+    <v-container>
       <v-row>
         <v-col class="user-info-container" align="center">
           <v-row>  <!-- title -->
             <v-col>
-              <div >
+              <div>
                 <h2>
                   <v-icon color="#004948">mdi-clipboard-list-outline</v-icon>
                   کوییز
@@ -20,7 +20,8 @@
           <v-card class="mainCard">
             <v-row>
               <v-col>
-                <v-card class="cardShape" style="margin-left: 2rem; margin-right: 2rem;margin-top: 2rem;margin-bottom: 2rem">
+                <v-card class="cardShape"
+                        style="margin-left: 2rem; margin-right: 2rem;margin-top: 2rem;margin-bottom: 2rem">
                   <v-row> <!-- 1 -->
                     <v-col>
                       <p class="mainText">نوع کوییز: <span class="innerText">کوییز پیش فرض</span></p>
@@ -28,22 +29,25 @@
                   </v-row>
                   <v-row> <!-- 2 -->
                     <v-col>
-                      <p style="margin-top: -2rem" class="mainText">زمان کوییز:  <span class="innerText">۵ دقیقه</span></p>
+                      <p style="margin-top: -2rem" class="mainText">زمان کوییز: <span class="innerText">۵ دقیقه</span>
+                      </p>
                     </v-col>
                   </v-row>
                   <v-row> <!-- 3 -->
                     <v-col>
-                      <p style="margin-top: -2rem" class="mainText">تعداد سوالات:  <span class="innerText">۵ سوال</span></p>
+                      <p style="margin-top: -2rem" class="mainText">تعداد سوالات: <span class="innerText">۵ سوال</span>
+                      </p>
                     </v-col>
                   </v-row>
                   <v-row> <!-- 4 -->
                     <v-col>
-                      <p style="margin-top: -2rem" class="mainText">نوع سوالات:  <span class="innerText">تستی</span></p>
+                      <p style="margin-top: -2rem" class="mainText">نوع سوالات: <span class="innerText">تستی</span></p>
                     </v-col>
                   </v-row>
                   <v-row> <!-- 5 -->
                     <v-col>
-                      <p style="margin-top: -2rem" class="mainText">نوع کوییز: <span class="innerText">کوییز پیش فرض</span></p>
+                      <p style="margin-top: -2rem" class="mainText">نوع کوییز: <span
+                        class="innerText">کوییز پیش فرض</span></p>
                     </v-col>
                   </v-row>
                   <v-row>
@@ -67,33 +71,46 @@
 <script>
 export default {
   name: "quiz",
-  data(){
-    return{
-
-    }
+  data() {
+    return {}
   },
-  methods:{
-    participate(){
-      this.$router.push('/forms/quizForm')
+  methods: {
+    async participate() {
+      this.$nuxt.$loading.start()
+      try {
+        let data = await this.$axios.$post('/quiz/create/')
+        this.$router.push('/forms/quizForm/' + data.pk)
+      } catch (e) {
+        this.$notify({
+          group: 'foo',
+          type: 'error',
+
+          title: e.response.data.error,
+        });
+      }
+      this.$nuxt.$loading.finish()
+
     }
   }
 }
 </script>
 
 <style scoped>
-.user-info-container{
+.user-info-container {
   border: 2px #004948 solid;
   border-radius: 15px;
   padding: 20px;
 }
-hr{
+
+hr {
   background: #004948;
   height: 2px;
   opacity: 1;
   border-radius: 3px;
   min-width: 350px;
 }
-h2{
+
+h2 {
   text-align: center;
   font-size: 20px;
   font-weight: bold;
@@ -101,6 +118,7 @@ h2{
   word-break: keep-all;
   color: #004948;
 }
+
 .cardShape {
   background: #C5E3E3;
   box-shadow: inset -18px -18px 30px #E0F0F0, inset 18px 18px 30px #98CDCD !important;
@@ -109,23 +127,27 @@ h2{
   padding-bottom: 2em;
 
 }
-.mainCard{
+
+.mainCard {
   background: #C5E3E3;
   /* sabz-bar */
 
   box-shadow: -18px -18px 30px #E0F0F0, 18px 18px 30px #98CDCD;
   border-radius: 15px;
 }
-.mainText{
+
+.mainText {
   color: #004948;
   font-family: 'Vazir';
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
 }
-.innerText{
+
+.innerText {
   font-weight: 900 !important;
 }
+
 .edit_btn {
   box-shadow: inset 0px -4px 4px rgba(255, 252, 252, 0.25);
   filter: drop-shadow(0px 8px 10px rgba(0, 146, 143, 0.25));
