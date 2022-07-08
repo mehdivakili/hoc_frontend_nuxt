4c505d62761425ef727670d54aa0a0214c11bf93
Hosseinreza <hrakbari77@gmail.com>
2022-07-08 19:31:44 +0430

    Finish top side

--- pages/account/GroupSelection.vue
+++ pages/account/GroupSelection.vue
@@ -6,9 +6,9 @@
         <v-col>
           <div >
             <h2>
-              <v-icon>mdi-alert-circle-outline</v-icon>
+              <v-icon color="#004948">mdi-account-group-outline </v-icon>
               انتخاب گروه همایش
-              <v-icon>mdi-alert-circle-outline</v-icon>
+              <v-icon color="#004948">mdi-account-group-outline</v-icon>
 
             </h2>
           </div>
@@ -17,7 +17,61 @@
       <hr>
 
       <v-row> <!-- Body -->
+        <v-col>
+          <v-row> <!-- groups row -->
+            <v-col v-for="item in items">
+              <v-simple-table style="border: 1px solid #004948;border-radius: 10px;">
+                <template>
+                  <tr  > <!-- 1 -->
+                    <th>
+                      <p>شماره گروه</p>
+                    </th>
+                    <td >
+                      <p>{{ item["groupNumber"] }}</p>
+                    </td>
+                  </tr>
+                  <tr > <!-- 2 -->
+                    <th>
+                      <p>جنسیت</p>
+                    </th>
+                    <td>
+                      <p>{{ item["gender"] }}</p>
+                    </td>
+                  </tr>
+                  <tr > <!-- 3 -->
+                    <th>
+                      <p>روز اول</p>
+                    </th>
+                    <td>
+                      <p style="width: 5rem;">{{ item["dayOne"] }}</p>
+                    </td>
+                  </tr>
+                  <tr > <!-- 4 -->
+                    <th>
+                      <p>روز دوم</p>
+                    </th>
+                    <td>
+                      <p style="width: 5rem">{{ item["dayTwo"] }}</p>
+                    </td>
+                  </tr>
+                  <tr > <!-- 5 -->
+                    <th>
+                      <p>ظرفیت</p>
+                    </th>
+                    <td>
+                      <p>{{ item["capacity"] }}</p>
+                    </td>
+                  </tr>
+                </template>
+              </v-simple-table>
+            </v-col>
 
+          </v-row>
+          <v-row> <!-- buttons -->
+
+
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
 
@@ -27,7 +81,52 @@
 
 <script>
 export default {
-  name: "GroupSelection"
+  name: "GroupSelection",
+  data(){
+    return{
+      items:[
+        {
+          groupNumber : 1,
+          gender: 'دختر',
+          dayOne: '۲۵ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          dayTwo: '۲۶ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          capacity: '۵۰ نفر'
+        },
+        {
+          groupNumber : 2,
+          gender: 'دختر',
+          dayOne: '۲۵ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          dayTwo: '۲۶ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          capacity: '۵۰ نفر'
+        },
+        {
+          groupNumber : 3,
+          gender: 'دختر',
+          dayOne: '۲۵ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          dayTwo: '۲۶ تیر\n' +
+            'ساعت ۹ تا ۱۰:۳۰\n' +
+            'و\n' +
+            'ساعت ۱۱ تا ۱۲:۳۰',
+          capacity: '۵۰ نفر'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -43,6 +142,7 @@ h2{
   font-weight: bold;
   margin: 20px 0 40px 0;
   word-break: keep-all;
+  color: #004948;
 }
 hr{
   background: #004948;
@@ -51,23 +151,18 @@ hr{
   border-radius: 3px;
   min-width: 350px;
 }
-.user-info{
-  display: flex;
-  flex-wrap: wrap;
-  p{
-    min-width: 300px;
-    width: calc(50% - 20px);
-    padding-right: 10%;
-    word-break: keep-all;
-    margin: 10px;
-    text-align: right;
-  }
-  @media screen and (max-width: 1000px){
-    p{
-      width: 100%;
-    }
-  }
-
+th p{
+  margin-right: 1em;
+  margin-top:1em;
+}
+td p {
+  margin-top: 1em;
+}
+tr:nth-child(even) {
+  background-color: #C5E3E3;
+}
+tr {
+  text-align: center;
 }
 @media screen  and (max-width: 420px){
   hr{
