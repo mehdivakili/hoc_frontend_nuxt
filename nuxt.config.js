e0a849716d3e86be588826eed7ed555f429efb2d
mehdivakili <0mvakilin0@gmail.com>
2022-07-14 17:32:29 +0430

    connect it to api

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,7 +70,6 @@ export default {
     '@nuxt/content',
 
     '@nuxtjs/auth-next',
-    '@nuxtjs/markdownit',
 
     "nuxt-compress"
 
@@ -79,7 +78,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'https://api.hocshirazu.ir/api/',
   },
   markdownit: {
     preset: 'default',
